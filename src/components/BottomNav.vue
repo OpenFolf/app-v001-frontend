a16841ce5491763c0460c24c0ad5ec4f67e7c063
Aegir Aexx <aegir.tomasson@gmail.com>
2020-03-17 11:34:22 +0000

    Removed all the pre-built stuff. Added our logo and images.

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>bottom navigation placeholder</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        dummy: "value",
+      };
+    },
+  };
+</script>
