e1e5473d2e4473e24a57924fc2eb1efdb37f199e
dagurdagur <34142077+dagurdagur@users.noreply.github.com>
2020-03-18 23:53:54 +0000

    graphql init

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -37,3 +37,708 @@ export const deleteTodo = /* GraphQL */ `
     }
   }
 `;
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      games {
+        id
+        course {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+        owner {
+          id
+          username
+          email
+        }
+        players {
+          id
+          username
+          email
+        }
+      }
+      friends {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+      favoritecourses {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      games {
+        id
+        course {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+        owner {
+          id
+          username
+          email
+        }
+        players {
+          id
+          username
+          email
+        }
+      }
+      friends {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+      favoritecourses {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      username
+      email
+      games {
+        id
+        course {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+        owner {
+          id
+          username
+          email
+        }
+        players {
+          id
+          username
+          email
+        }
+      }
+      friends {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+      favoritecourses {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+    }
+  }
+`;
+export const createGame = /* GraphQL */ `
+  mutation CreateGame(
+    $input: CreateGameInput!
+    $condition: ModelGameConditionInput
+  ) {
+    createGame(input: $input, condition: $condition) {
+      id
+      course {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+      owner {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+      players {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+    }
+  }
+`;
+export const updateGame = /* GraphQL */ `
+  mutation UpdateGame(
+    $input: UpdateGameInput!
+    $condition: ModelGameConditionInput
+  ) {
+    updateGame(input: $input, condition: $condition) {
+      id
+      course {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+      owner {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+      players {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+    }
+  }
+`;
+export const deleteGame = /* GraphQL */ `
+  mutation DeleteGame(
+    $input: DeleteGameInput!
+    $condition: ModelGameConditionInput
+  ) {
+    deleteGame(input: $input, condition: $condition) {
+      id
+      course {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+      owner {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+      players {
+        id
+        username
+        email
+        games {
+          id
+        }
+        friends {
+          id
+          username
+          email
+        }
+        favoritecourses {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+      }
+    }
+  }
+`;
+export const createCourse = /* GraphQL */ `
+  mutation CreateCourse(
+    $input: CreateCourseInput!
+    $condition: ModelCourseConditionInput
+  ) {
+    createCourse(input: $input, condition: $condition) {
+      id
+      name
+      holes {
+        id
+        no
+        nickname
+        course {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+        par
+      }
+      courselength
+      yearestablished
+      parinfo
+      teetype
+      baskettype
+      description
+      streetaddress
+      postalcode
+      location
+    }
+  }
+`;
+export const updateCourse = /* GraphQL */ `
+  mutation UpdateCourse(
+    $input: UpdateCourseInput!
+    $condition: ModelCourseConditionInput
+  ) {
+    updateCourse(input: $input, condition: $condition) {
+      id
+      name
+      holes {
+        id
+        no
+        nickname
+        course {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+        par
+      }
+      courselength
+      yearestablished
+      parinfo
+      teetype
+      baskettype
+      description
+      streetaddress
+      postalcode
+      location
+    }
+  }
+`;
+export const deleteCourse = /* GraphQL */ `
+  mutation DeleteCourse(
+    $input: DeleteCourseInput!
+    $condition: ModelCourseConditionInput
+  ) {
+    deleteCourse(input: $input, condition: $condition) {
+      id
+      name
+      holes {
+        id
+        no
+        nickname
+        course {
+          id
+          name
+          courselength
+          yearestablished
+          parinfo
+          teetype
+          baskettype
+          description
+          streetaddress
+          postalcode
+          location
+        }
+        par
+      }
+      courselength
+      yearestablished
+      parinfo
+      teetype
+      baskettype
+      description
+      streetaddress
+      postalcode
+      location
+    }
+  }
+`;
+export const createHole = /* GraphQL */ `
+  mutation CreateHole(
+    $input: CreateHoleInput!
+    $condition: ModelHoleConditionInput
+  ) {
+    createHole(input: $input, condition: $condition) {
+      id
+      no
+      nickname
+      course {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+      par
+    }
+  }
+`;
+export const updateHole = /* GraphQL */ `
+  mutation UpdateHole(
+    $input: UpdateHoleInput!
+    $condition: ModelHoleConditionInput
+  ) {
+    updateHole(input: $input, condition: $condition) {
+      id
+      no
+      nickname
+      course {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+      par
+    }
+  }
+`;
+export const deleteHole = /* GraphQL */ `
+  mutation DeleteHole(
+    $input: DeleteHoleInput!
+    $condition: ModelHoleConditionInput
+  ) {
+    deleteHole(input: $input, condition: $condition) {
+      id
+      no
+      nickname
+      course {
+        id
+        name
+        holes {
+          id
+          no
+          nickname
+          par
+        }
+        courselength
+        yearestablished
+        parinfo
+        teetype
+        baskettype
+        description
+        streetaddress
+        postalcode
+        location
+      }
+      par
+    }
+  }
+`;
