a4aa0f7921b81cf11cdadb2fafdde79bab865ede
Aegir Aexx <aegir.tomasson@gmail.com>
2020-03-19 03:36:51 +0000

    Added a placeholder component to wrap all other auth components

---
+++ src/components/auth/Authenticator.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- TODO: IMPLEMENT A WRAPPER COMPONENT TO WRAP ALL THE OTHERS -->
+  </div>
+</template>
+
+<script>
+  export default {
+    dummy: "value",
+  };
+</script>
