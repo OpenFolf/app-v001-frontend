a65615994458596f7c240389dc85731030fe413f
Aegir Aexx <aegir.tomasson@gmail.com>
2020-03-19 03:36:08 +0000

    Added a persistent topbar while logged in

---
+++ src/components/TopBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar color="#41b883" app>
+    <router-link :to="{ name: 'home' }">
+      <v-img
+        class="mr-2"
+        :src="require('@/assets/basket_white.png')"
+        max-height="40"
+        max-width="40"
+        contain
+      />
+    </router-link>
+    <v-spacer />
+    <v-toolbar-title class="headline mr-5">{{ pageName }}</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        pageName: "Title",
+      };
+    },
+  };
+</script>
